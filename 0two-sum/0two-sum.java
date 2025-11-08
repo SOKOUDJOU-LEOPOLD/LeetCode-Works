98c7935e11777d38df7e53ef1cfcdb6ff3d45b5d
SOKOUDJOU Leopold <109520753+SOKOUDJOU-LEOPOLD@users.noreply.github.com>
2025-11-08 13:31:10 -0500

    Time: 5 ms, Memory: 46.5 MB - LeetHub

---
+++ 0two-sum/0two-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    private int size = 2;
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[this.size];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int compl = target-nums[i];
+            if(map.containsKey(compl)){
+                res[0] = map.get(compl);
+                res[1] = i;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+        
+    }
+}
+
+
+
+/*
+-> create an array of int result to hold the result
+-> create a hashmap (value, index) to store the value and index of any element I traverse
+-> when I traverse the array nums i check if target-nums[i] is inside the hashmap.
+    -> if yes -> I return the two index added inside the result array.
+    -> else -> add that inside the map.
+
+-> it is sure that we will have a unique result.
+
+*/
