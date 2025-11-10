41f232932ea9a3d0cf9eb58bc609f57ab44bca77
SOKOUDJOU Leopold <109520753+SOKOUDJOU-LEOPOLD@users.noreply.github.com>
2025-11-10 06:33:47 -0500

    Time: 4 ms, Memory: 45.6 MB - LeetHub

---
+++ sparse-matrix-multiplication/sparse-matrix-multiplication.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] multiply(int[][] mat1, int[][] mat2) {
+        int m = mat1.length;    // row
+        int k = mat1[0].length;
+        int n = mat2[0].length;   // col
+        
+        int[][] res = new int[m][n];
+        for(int i =0; i<m; i++){
+            for(int j=0; j<n; j++){
+                res[i][j] = 0;
+                for(int t =0; t<k; t++){
+                    res[i][j] += mat1[i][t]*mat2[t][j]; 
+                }
+            }
+        }
+        
+        return res;
+        
+    }
+}
