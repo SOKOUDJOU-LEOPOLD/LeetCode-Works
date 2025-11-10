580fe80669408d92c37141c3f6d58e7c9609767a
SOKOUDJOU Leopold <109520753+SOKOUDJOU-LEOPOLD@users.noreply.github.com>
2025-11-10 13:08:29 -0500

    Time: 12 ms, Memory: 45.8 MB - LeetHub

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -9,20 +9,23 @@ class Solution {
     }
     
     public void dfs(int n, int open_count, int close_count, String cur){
+        if(open_count > n){
+            return;
+        }
         
         if(n == open_count && open_count == close_count){
             res.add(cur);
             System.out.println(cur);
         }
         
-        if(open_count<=n){
+        // if(open_count<=n){
             if(open_count>close_count){  // open or close 
                 dfs(n, open_count+1, close_count, cur+"(");
                 dfs(n, open_count, close_count+1, cur+ ")") ;          
             }else{ // open
                 dfs(n, open_count +1, close_count, cur+"(");
             }
-        }
+        // }
     
     }
 }
