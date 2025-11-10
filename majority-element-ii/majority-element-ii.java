8cdb9d6c8e4cffbfee7964b77768cb095409239f
SOKOUDJOU Leopold <109520753+SOKOUDJOU-LEOPOLD@users.noreply.github.com>
2025-11-10 04:39:36 -0500

    Time: 18 ms, Memory: 51.9 MB - LeetHub

---
+++ majority-element-ii/majority-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            if(map.get(nums[i]) > n/3){
+                set.add(nums[i]);
+            }
+            
+        }
+        
+        for(int i: set){
+            res.add(i);
+        }
+        
+        return res;
+        
+    }
+}
