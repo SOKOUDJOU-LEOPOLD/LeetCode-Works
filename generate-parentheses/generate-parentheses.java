4725c5d2fdc0bee0fb80d6bc6c333804238eb5ac
SOKOUDJOU Leopold <109520753+SOKOUDJOU-LEOPOLD@users.noreply.github.com>
2025-11-10 13:07:13 -0500

    Time: 10 ms, Memory: 45.5 MB - LeetHub

---
+++ generate-parentheses/generate-parentheses.java
@@ -0,0 +1,39 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        boolean flag = false;
+        int open_count = 0;
+        int close_count = 0;
+        dfs(n, open_count, close_count, "");
+        return res;
+    }
+    
+    public void dfs(int n, int open_count, int close_count, String cur){
+        
+        if(n == open_count && open_count == close_count){
+            res.add(cur);
+            System.out.println(cur);
+        }
+        
+        if(open_count<=n){
+            if(open_count>close_count){  // open or close 
+                dfs(n, open_count+1, close_count, cur+"(");
+                dfs(n, open_count, close_count+1, cur+ ")") ;          
+            }else{ // open
+                dfs(n, open_count +1, close_count, cur+"(");
+            }
+        }
+    
+    }
+}
+
+/*
+                                        ""
+                               "("
+                          "()"     "(("
+
+
+
+
+
+*/
